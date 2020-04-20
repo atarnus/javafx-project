ee3e18cfac51cda7779eb42c4cc32e78826318fe
Katariina Pohto <katariina.pohto@gmail.com>
2020-04-20 20:04:52 +0300

    window title changed.

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -6,7 +6,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Kassu");
+        stage.setTitle("JavaFX Code Editor");
         stage.show();
     }
     public static void main(String args[]) {
