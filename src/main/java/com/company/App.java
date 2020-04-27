9316ba79bf51b2284bebeac618488713970d6208
Katariina Pohto <katariina.pohto@gmail.com>
2020-04-27 21:02:11 +0300

    window style, size and first button

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,14 +1,42 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.*;
 
 public class App extends Application {
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("JavaFX Code Editor");
-        stage.show();
+
+    public App() {
+        System.out.println("constructor");
     }
+
+    @Override
+    public void init() {
+        System.out.println("init");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("stop");
+    }
+
+    @Override
+    public void start(Stage window) {
+        window.setTitle("JavaFX Code Editor");
+        window.initStyle(StageStyle.UNIFIED);
+        window.setWidth(640);
+        window.setHeight(480);
+        window.centerOnScreen();
+
+        Scene scene = new Scene(new Button("Click!"));
+        window.setScene(scene);
+
+        window.show();
+        System.out.println("start");
+    }
+
     public static void main(String args[]) {
         launch(args);
     }
