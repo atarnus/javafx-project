438eb94f6b46a3ea49c1a7675e7521208689d45e
Katariina Pohto <katariina.pohto@gmail.com>
2020-04-29 01:30:03 +0300

    added toolbar, open/save file, tab=4 spaces

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -8,11 +8,18 @@ import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 
 public class App extends Application {
+
+    private TextArea textArea;
+    private Stage window;
 
     public App() {
         System.out.println("constructor");
@@ -30,16 +37,37 @@ public class App extends Application {
 
     @Override
     public void start(Stage window) {
+        FileChooser fc = new FileChooser();
+
         window.setTitle("JavaFX Code Editor");
         window.initStyle(StageStyle.UNIFIED);
         window.centerOnScreen();
 
         BorderPane pane = new BorderPane();
+
         Scene scene = new Scene(pane, 640, 480);
         MenuBar menuBar = new MenuBar();
+        ToolBar toolBar = new ToolBar();
         TextArea textArea = new TextArea("");
-        pane.setTop(menuBar);
+
+        VBox paneTop = new VBox(menuBar, toolBar);
+        pane.setTop(paneTop);
         pane.setCenter(textArea);
+
+        // Toolbar buttons
+        ColorPicker colorPicker = new ColorPicker();
+        Button clear = new Button("Clear Text");
+        Button button2 = new Button();
+        toolBar.getItems().addAll(clear);
+        clear.setOnAction(actionEvent -> textArea.setText(""));
+
+        // On Text Area: Tab -> 4 spaces
+        textArea.setOnKeyPressed(keyEvent -> {
+                if (keyEvent.getCode() == KeyCode.TAB) {
+                    int x = textArea.getCaretPosition();
+                    textArea.replaceText(x-1, x, "    ");
+                }
+        });
 
         Menu file = new Menu("File");
         Menu edit = new Menu("Edit");
@@ -69,35 +97,44 @@ public class App extends Application {
         MenuItem menuAbout = new MenuItem("About App");
         SeparatorMenuItem separator = new SeparatorMenuItem();
 
-        file.getItems().add(menuNew);
-        file.getItems().add(menuOpen);
-        file.getItems().add(menuSave);
-        file.getItems().add(separator);
-        file.getItems().add(menuExit);
-        edit.getItems().add(menuCut);
-        edit.getItems().add(menuCopy);
-        edit.getItems().add(menuPaste);
+        file.getItems().addAll(menuNew, menuOpen, menuSave, separator, menuExit);
+        edit.getItems().addAll(menuCut, menuCopy, menuPaste);
         run.getItems().add(menuRun);
         about.getItems().add(menuAbout);
 
-        menuBar.getMenus().add(file);
-        menuBar.getMenus().add(edit);
-        menuBar.getMenus().add(run);
-        menuBar.getMenus().add(about);
+        menuBar.getMenus().addAll(file, edit, run, about);
 
+        // Menu Open File
+        menuOpen.setOnAction(e -> {
+            File openFile = fc.showOpenDialog(window);
+            Path openpath = openFile.toPath();
+            if (openFile != null) {
+                try {
+                    String content = Files.readString(openpath);
+                    textArea.setText(content);
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+
+        // Menu Save File
+        menuSave.setOnAction(e -> {
+            File saveFile = fc.showSaveDialog(window);
+            Path savePath = saveFile.toPath();
+            if (saveFile != null) {
+                try {
+                    String savecontent = textArea.getText();
+                    Files.writeString(savePath, savecontent);
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
 
         window.setScene(scene);
-
-        /*
-        Button clear = new Button("Clear");
-        clear.setOnAction(actionEvent -> textArea.setText(""));
-        */
-
-
         window.show();
         System.out.println("start");
-
-
     }
 
     public static void main(String args[]) {
