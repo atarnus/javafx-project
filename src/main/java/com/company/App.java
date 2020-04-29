79ba10173d3c648a6572e014db7a38a9f45b01c5
Katariina Pohto <katariina.pohto@gmail.com>
2020-04-30 01:23:27 +0300

    about dialog, exit/clear confirmation windows, some cleanup

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -17,6 +17,7 @@ import javafx.stage.StageStyle;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 
 public class App extends Application {
@@ -24,33 +25,26 @@ public class App extends Application {
     private Stage window;
     TextArea textArea = new TextArea("");
 
-    public App() {
-        System.out.println("constructor");
-    }
-
     @Override
-    public void init() {
-        System.out.println("init");
-    }
-
-    @Override
-    public void stop() {
-        System.out.println("stop");
-    }
-
-    @Override
-    public void start(Stage window) throws FileNotFoundException {
-        window.setTitle("Text Editor");                             // Window title and style
+    public void start(Stage window) {
+        window.setTitle("Text Editor");                                 // Window title and style
         window.initStyle(StageStyle.UNIFIED);
         window.centerOnScreen();
 
-        BorderPane pane = new BorderPane();                         // Setting up BorderPane and Bars
+        BorderPane pane = new BorderPane();                             // Setting up BorderPane and Bars
         Scene scene = new Scene(pane, 640, 480);
         MenuBar menuBar = new MenuBar();
         ToolBar toolBar = new ToolBar();
         VBox paneTop = new VBox(menuBar, toolBar);
         pane.setTop(paneTop);
         pane.setCenter(textArea);
+
+        textArea.setOnKeyPressed(keyEvent -> {                      // On Text Area: Tab -> 4 Spaces
+            if (keyEvent.getCode() == KeyCode.TAB) {
+                int x = textArea.getCaretPosition();
+                textArea.replaceText(x - 1, x, "    ");
+            }
+        });
 
         Image imgSave = new Image("images/iconsave.png");           // Toolbar icons
         Image imgOpen = new Image("images/iconopen.png");
@@ -64,14 +58,16 @@ public class App extends Application {
         Button cut = new Button();
         Button copy = new Button();
         Button paste = new Button();
-        clear.setPrefSize(75, 35);
+        toolBar.getItems().addAll(save, open, new Separator(), cut, copy, paste, new Separator(), clear);
+
+        clear.setPrefSize(75, 35);                              // Button sizes
         open.setPrefSize(40, 35);
         save.setPrefSize(40, 35);
         cut.setPrefSize(40, 35);
         copy.setPrefSize(40, 35);
         paste.setPrefSize(40, 35);
 
-        open.setGraphic(new ImageView(imgOpen));                        // Setting icons to buttons
+        open.setGraphic(new ImageView(imgOpen));                     // Setting icons to buttons
         save.setGraphic(new ImageView(imgSave));
         cut.setGraphic(new ImageView(imgCut));
         copy.setGraphic(new ImageView(imgCopy));
@@ -83,21 +79,12 @@ public class App extends Application {
         copy.setTooltip(new Tooltip("Copy Text"));
         paste.setTooltip(new Tooltip("Paste Text"));
 
-        toolBar.getItems().addAll(save, open, new Separator(), cut, copy, paste, new Separator(), clear);
-
-        clear.setOnAction(ActionEvent -> textArea.setText(""));     // Button Actions
+        clear.setOnAction(this::clear);                             // Button Actions
         open.setOnAction(this::open);
         save.setOnAction(this::save);
         cut.setOnAction(actionEvent -> textArea.cut());
         copy.setOnAction(actionEvent -> textArea.copy());
         paste.setOnAction(actionEvent -> textArea.paste());
-
-        textArea.setOnKeyPressed(keyEvent -> {                      // On Text Area: Tab -> 4 Spaces
-            if (keyEvent.getCode() == KeyCode.TAB) {
-                int x = textArea.getCaretPosition();
-                textArea.replaceText(x - 1, x, "    ");
-            }
-        });
 
         Menu file = new Menu("File");                           // Menus
         Menu edit = new Menu("Edit");
@@ -122,21 +109,54 @@ public class App extends Application {
         menuCopy.setAccelerator(new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN));
         menuPaste.setAccelerator(new KeyCodeCombination(KeyCode.V, KeyCombination.CONTROL_DOWN));
 
-        menuExit.setOnAction(actionEvent -> System.exit(0));    // Menu Actions
-        menuCut.setOnAction(actionEvent -> textArea.cut());
+        menuCut.setOnAction(actionEvent -> textArea.cut());         // Menu Actions
         menuCopy.setOnAction(actionEvent -> textArea.copy());
         menuPaste.setOnAction(actionEvent -> textArea.paste());
         menuOpen.setOnAction(this::open);
         menuSave.setOnAction(this::save);
+        menuExit.setOnAction(this::exit);
+        menuAbout.setOnAction(actionEvent -> {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("About");
+            alert.setHeaderText(null);
+            alert.setContentText("This is a small project done as a part of Code Training Camp course. \n\nButton icons from http://icons8.com.");
+            alert.showAndWait();
+        });
 
         window.setScene(scene);
         window.show();
     }
 
+    private void clear(ActionEvent actionEvent) {                   // Clear Text Method with Confirmation Window
+        Alert clearAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        clearAlert.setTitle("Clear Text");
+        clearAlert.setHeaderText(null);
+        clearAlert.setContentText("Are you sure want to clear all text?");
+        Optional<ButtonType> result = clearAlert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            textArea.setText("");
+        }
+    }
+
+    private void exit(ActionEvent event) {                          // Exit Method with Confirmation Window
+        Alert exitAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        exitAlert.setTitle("Exit");
+        exitAlert.setHeaderText(null);
+        exitAlert.setContentText("Are you sure want to exit? Any unsaved information will be lost.");
+        Optional<ButtonType> result = exitAlert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            System.exit(0);
+        }
+    }
+
     public void open(ActionEvent event) {                           // Open File Method
-        FileChooser fc = new FileChooser();
+        FileChooser fc = new FileChooser();                         // File Chooser with Extension Filter
+        fc.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Java Files", "*.java"),
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
         File openFile = fc.showOpenDialog(window);
-        if (openFile != null) {
+        if (openFile != null) {                                     // If file is chosen, set its content to Text Area
             Path openPath = openFile.toPath();
             try {
                 String content = Files.readString(openPath);
@@ -148,9 +168,12 @@ public class App extends Application {
     }
 
     public void save(ActionEvent event) {                           // Save File Method
-        FileChooser fc = new FileChooser();
+        FileChooser fc = new FileChooser();                         // File Chooser with Extension Filter
+        fc.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Java Files", "*.java"),
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"));
         File saveFile = fc.showSaveDialog(window);
-        if (saveFile != null) {
+        if (saveFile != null) {                                     // If file is chosen, save Text Area content to it
             Path savePath = saveFile.toPath();
             try {
                 String saveContent = textArea.getText();
