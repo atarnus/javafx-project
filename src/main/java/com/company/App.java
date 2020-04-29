3a27b5e695d841fad97466f30d9c4fd594783528
Katariina Pohto <katariina.pohto@gmail.com>
2020-04-30 00:19:39 +0300

    fixed open/save cancel error, added buttons /w icons and tooltips, tidying

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,8 +1,11 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
@@ -18,8 +21,8 @@ import java.nio.file.Path;
 
 public class App extends Application {
 
-    private TextArea textArea;
     private Stage window;
+    TextArea textArea = new TextArea("");
 
     public App() {
         System.out.println("constructor");
@@ -36,105 +39,126 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage window) {
-        FileChooser fc = new FileChooser();
-
-        window.setTitle("JavaFX Code Editor");
+    public void start(Stage window) throws FileNotFoundException {
+        window.setTitle("Text Editor");                             // Window title and style
         window.initStyle(StageStyle.UNIFIED);
         window.centerOnScreen();
 
-        BorderPane pane = new BorderPane();
-
+        BorderPane pane = new BorderPane();                         // Setting up BorderPane and Bars
         Scene scene = new Scene(pane, 640, 480);
         MenuBar menuBar = new MenuBar();
         ToolBar toolBar = new ToolBar();
-        TextArea textArea = new TextArea("");
-
         VBox paneTop = new VBox(menuBar, toolBar);
         pane.setTop(paneTop);
         pane.setCenter(textArea);
 
-        // Toolbar buttons
-        ColorPicker colorPicker = new ColorPicker();
-        Button clear = new Button("Clear Text");
-        Button button2 = new Button();
-        toolBar.getItems().addAll(clear);
-        clear.setOnAction(actionEvent -> textArea.setText(""));
+        Image imgSave = new Image("images/iconsave.png");           // Toolbar icons
+        Image imgOpen = new Image("images/iconopen.png");
+        Image imgCut = new Image("images/iconcut.png");
+        Image imgCopy = new Image("images/iconcopy.png");
+        Image imgPaste = new Image("images/iconpaste.png");
 
-        // On Text Area: Tab -> 4 spaces
-        textArea.setOnKeyPressed(keyEvent -> {
-                if (keyEvent.getCode() == KeyCode.TAB) {
-                    int x = textArea.getCaretPosition();
-                    textArea.replaceText(x-1, x, "    ");
-                }
+        Button clear = new Button("Clear Text");                    // Toolbar buttons
+        Button open = new Button();
+        Button save = new Button();
+        Button cut = new Button();
+        Button copy = new Button();
+        Button paste = new Button();
+        clear.setPrefSize(75, 35);
+        open.setPrefSize(40, 35);
+        save.setPrefSize(40, 35);
+        cut.setPrefSize(40, 35);
+        copy.setPrefSize(40, 35);
+        paste.setPrefSize(40, 35);
+
+        open.setGraphic(new ImageView(imgOpen));                        // Setting icons to buttons
+        save.setGraphic(new ImageView(imgSave));
+        cut.setGraphic(new ImageView(imgCut));
+        copy.setGraphic(new ImageView(imgCopy));
+        paste.setGraphic(new ImageView(imgPaste));
+
+        open.setTooltip(new Tooltip("Open File"));                  // Button tooltips
+        save.setTooltip(new Tooltip("Save File"));
+        cut.setTooltip(new Tooltip("Cut Text"));
+        copy.setTooltip(new Tooltip("Copy Text"));
+        paste.setTooltip(new Tooltip("Paste Text"));
+
+        toolBar.getItems().addAll(save, open, new Separator(), cut, copy, paste, new Separator(), clear);
+
+        clear.setOnAction(ActionEvent -> textArea.setText(""));     // Button Actions
+        open.setOnAction(this::open);
+        save.setOnAction(this::save);
+        cut.setOnAction(actionEvent -> textArea.cut());
+        copy.setOnAction(actionEvent -> textArea.copy());
+        paste.setOnAction(actionEvent -> textArea.paste());
+
+        textArea.setOnKeyPressed(keyEvent -> {                      // On Text Area: Tab -> 4 Spaces
+            if (keyEvent.getCode() == KeyCode.TAB) {
+                int x = textArea.getCaretPosition();
+                textArea.replaceText(x - 1, x, "    ");
+            }
         });
 
-        Menu file = new Menu("File");
+        Menu file = new Menu("File");                           // Menus
         Menu edit = new Menu("Edit");
-        Menu run = new Menu("Run");
         Menu about = new Menu("About");
+        menuBar.getMenus().addAll(file, edit, about);
 
-        MenuItem menuNew = new MenuItem("New");
-        menuNew.setAccelerator(new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_DOWN));
-        MenuItem menuOpen = new MenuItem("Open..");
-        menuOpen.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
+        MenuItem menuOpen = new MenuItem("Open..");             // Menu Items
         MenuItem menuSave = new MenuItem("Save..");
-        menuSave.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN));
         MenuItem menuExit = new MenuItem("Exit");
-        menuExit.setOnAction(actionEvent -> System.exit(0));
-        menuExit.setAccelerator(new KeyCodeCombination(KeyCode.E, KeyCombination.CONTROL_DOWN));
         MenuItem menuCut = new MenuItem("Cut");
-        menuCut.setOnAction(actionEvent -> textArea.cut());
-        menuCut.setAccelerator(new KeyCodeCombination(KeyCode.X, KeyCombination.CONTROL_DOWN));
         MenuItem menuCopy = new MenuItem("Copy");
-        menuCopy.setOnAction(actionEvent -> textArea.copy());
-        menuCopy.setAccelerator(new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN));
         MenuItem menuPaste = new MenuItem("Paste");
-        menuPaste.setOnAction(actionEvent -> textArea.paste());
-        menuPaste.setAccelerator(new KeyCodeCombination(KeyCode.V, KeyCombination.CONTROL_DOWN));
-        MenuItem menuRun = new MenuItem("Compile and Run..");
-        menuRun.setAccelerator(new KeyCodeCombination(KeyCode.R, KeyCombination.CONTROL_DOWN));
         MenuItem menuAbout = new MenuItem("About App");
-        SeparatorMenuItem separator = new SeparatorMenuItem();
-
-        file.getItems().addAll(menuNew, menuOpen, menuSave, separator, menuExit);
+        file.getItems().addAll(menuOpen, menuSave, new SeparatorMenuItem(), menuExit);
         edit.getItems().addAll(menuCut, menuCopy, menuPaste);
-        run.getItems().add(menuRun);
         about.getItems().add(menuAbout);
 
-        menuBar.getMenus().addAll(file, edit, run, about);
+        // Menu Shortcut Keys
+        menuOpen.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN));
+        menuSave.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN));
+        menuCut.setAccelerator(new KeyCodeCombination(KeyCode.X, KeyCombination.CONTROL_DOWN));
+        menuCopy.setAccelerator(new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN));
+        menuPaste.setAccelerator(new KeyCodeCombination(KeyCode.V, KeyCombination.CONTROL_DOWN));
 
-        // Menu Open File
-        menuOpen.setOnAction(e -> {
-            File openFile = fc.showOpenDialog(window);
-            Path openpath = openFile.toPath();
-            if (openFile != null) {
-                try {
-                    String content = Files.readString(openpath);
-                    textArea.setText(content);
-                } catch (IOException exception) {
-                    exception.printStackTrace();
-                }
-            }
-        });
-
-        // Menu Save File
-        menuSave.setOnAction(e -> {
-            File saveFile = fc.showSaveDialog(window);
-            Path savePath = saveFile.toPath();
-            if (saveFile != null) {
-                try {
-                    String savecontent = textArea.getText();
-                    Files.writeString(savePath, savecontent);
-                } catch (IOException exception) {
-                    exception.printStackTrace();
-                }
-            }
-        });
+        menuExit.setOnAction(actionEvent -> System.exit(0));    // Menu Actions
+        menuCut.setOnAction(actionEvent -> textArea.cut());
+        menuCopy.setOnAction(actionEvent -> textArea.copy());
+        menuPaste.setOnAction(actionEvent -> textArea.paste());
+        menuOpen.setOnAction(this::open);
+        menuSave.setOnAction(this::save);
 
         window.setScene(scene);
         window.show();
-        System.out.println("start");
+    }
+
+    public void open(ActionEvent event) {                           // Open File Method
+        FileChooser fc = new FileChooser();
+        File openFile = fc.showOpenDialog(window);
+        if (openFile != null) {
+            Path openPath = openFile.toPath();
+            try {
+                String content = Files.readString(openPath);
+                textArea.setText(content);
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    public void save(ActionEvent event) {                           // Save File Method
+        FileChooser fc = new FileChooser();
+        File saveFile = fc.showSaveDialog(window);
+        if (saveFile != null) {
+            Path savePath = saveFile.toPath();
+            try {
+                String saveContent = textArea.getText();
+                Files.writeString(savePath, saveContent);
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
     }
 
     public static void main(String args[]) {
